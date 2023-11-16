977e01d2b880cd7d973924762fd3ae817985e0a7
SaidHoff <103978168+SaidHoff@users.noreply.github.com>
2023-11-16 00:46:41 -0600

    slider.js

--- slider.js
+++ slider.js
@@ -2,6 +2,7 @@ $.get('get_images.php', function(images) {
 
     var html = '';
     images.forEach(function(image){
+        var imgPath = 'img/' + image; 
        html += '<div><img src="'+image+'"></div>';
     });
   
