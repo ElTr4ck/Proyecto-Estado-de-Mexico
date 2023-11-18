7f2ac172d688deaa9e760a6ae1173b39d2b5b7e4
ElTr4ck <jozafat112015@gmail.com>
2023-11-18 00:25:53 -0600

    Said

---
+++ get_images.php
@@ -0,0 +1,25 @@
+<?php
+
+// Conexión a la BD
+$conn = mysqli_connect("localhost", "root", "", "heladeria");
+// Verificar la conexión
+if (!$conn) {
+    die("La conexión a la base de datos falló: " . mysqli_connect_error());
+}
+// Consulta para obtener las imágenes
+$sql = "SELECT imagen FROM helados";
+$result = mysqli_query($conn, $sql);
+// Verificar si la consulta fue exitosa
+if (!$result) {
+    die("Error en la consulta: " . mysqli_error($conn));
+}
+// Armar arreglo con las imágenes
+$images = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $images[] =  $row['imagen'];
+}
+// Cerrar la conexión
+mysqli_close($conn);
+// Enviar como JSON
+echo json_encode($images);
+?>
