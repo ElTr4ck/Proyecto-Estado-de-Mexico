563d8d631cb079ab843203da2c6e637d520b4699
johnnylov01 <juaninflamas@gmail.com>
2023-11-18 01:40:34 -0600

    Hola senior locutor

--- login.php
+++ login.php
@@ -5,16 +5,14 @@ session_start();
 $usuario = $_POST['userAdmin'];
 $clave = $_POST['passAdmin'];
 
-$q = "SELECT COUNT(*) as contar from Administrador where Nombre = '$usuario' and Contraseña='$clave'";
+$q = "SELECT COUNT(*) as contar from Admin where nombreAdmin = '$usuario' and contra='$clave'";
 $consulta = mysqli_query($conexion, $q);
 $array = mysqli_fetch_array($consulta);
 
 if($array['contar']>0){
 $_SESSION['username']= $usuario;
 header("location: crud.php");
-
 }else{
-
     $_SESSION['accesoDenegado'] = true;
     header("location: adminLogin.php");
 }
